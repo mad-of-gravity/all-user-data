ec6df3143c176923f843dec0a62df73cb802eecc
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-02 12:54:32 +0200

    fetch data in useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Card } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import styles from "./App.module.scss";
+const { Meta } = Card;
 
 function App() {
+  const [registeredUsers, setRegisteredUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://dummyjson.com/users?limit=100");
+
+      if (response.ok) {
+        const regUsers = await response.json();
+        setRegisteredUsers(...registeredUsers, regUsers.users);
+      } else {
+        console.error("Error retrieving data..");
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.Container}>
+      {registeredUsers.map((user) => (
+        <Card
+          key={user.id}
+          style={{
+            width: 200,
+          }}
+          cover={<img alt="example" src={user.image} />}
+          actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+          ]}
         >
-          Learn React
-        </a>
-      </header>
+          <Meta title={user.firstName + " " + user.lastName} description="www.instagram.com" />
+        </Card>
+      ))}
     </div>
   );
 }
